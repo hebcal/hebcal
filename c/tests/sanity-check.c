f06d6d3287096c3f9d406ebb1b8e8fa684ec48fc
sadinoff <devnull@localhost>
2004-07-16 03:58:34 +0000

    added sanity

---
+++ c/tests/sanity-check.c
@@ -0,0 +1,64 @@
+/*
+  $Header$
+  sanity checker.  Looks for the "august tishri" problem
+*/
+
+#include <stdio.h>
+#include  <stdlib.h>
+#include "common.h"
+
+char * progname = "test-bug.cc";
+
+/* returns 1 */
+int reportIt(long d , date_t hebrew)
+{
+    date_t gregdate = abs2greg (d);
+    
+    printf ("%ld : [hm=%d hd=%d hy=%d], [gm=%d gd=%d gy=%d]\n",
+	    d,
+	    hebrew.mm,hebrew.dd,hebrew.yy,
+	    gregdate.mm,gregdate.dd,gregdate.yy);
+    return 1;
+}
+
+
+int main(int argc, char*argv[])
+{
+    long interval = 100000;
+    long intervalCount = 0;
+    
+    long start = 0L;        	/* 1 CE */
+    long finish = 2595363L;	/* ~7060 CE */
+    
+    int retVal =0;
+    long d;
+    
+    setlinebuf(stdout);
+    
+    if( argc > 1 )
+    {
+	if( 3 != argc )
+	{
+	    fprintf(stderr,"usage: %s [start finish]\n", argv[0]);
+	    exit(1);
+	}
+	sscanf(argv[1],"%ld",&start);
+	sscanf(argv[2],"%ld",&finish);
+    }
+    
+    for (d =start ; d < finish; d++)
+    {
+	if( 0 == intervalCount-- )
+	{
+	    printf("   %ld\n",d);
+	    intervalCount = interval-1;
+	}
+	date_t hebrew = abs2hebrew(d);
+	if( hebrew.dd < 0 
+	    || hebrew.mm < 0 
+	    || hebrew.yy < 0  )
+	  retVal += reportIt(d,hebrew);
+    }
+    printf("found %d failure cases\n",retVal);
+    exit( retVal );
+}
