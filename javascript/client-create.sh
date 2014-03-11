21b1d680179f847b310dd94b3aa4b44ebee0aeaa
Michael J. Radwin <michael@radwin.org>
2014-03-10 22:32:02 -0700

    JavaScript has moved from hebcal/hebcal to hebcal/hebcal-js

--- javascript/client-create.sh
+++
@@ -1,4 +0,0 @@
-browserify client.js > client.browser.js
-uglifyjs -m -v client.browser.js > client.min.js
-browserify client.noloc.src.js > client.noloc.js
-uglifyjs -m -v client.noloc.js > client.noloc.min.js
