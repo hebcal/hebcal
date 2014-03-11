f0147675d83bace457f483878ec38d66fed98427
Michael J. Radwin <michael@radwin.org>
2014-03-10 22:38:25 -0700

    Hebcal Perl has moved from hebcal/hebcal to hebcal/hebcal-perl. Hebcal Java has moved from hebcal/hebcal to hebcal/hebcal-java.

--- java/hebcalJ/src/main/resources/net/sf/hebcal_utf8/xcode.bat
+++
@@ -1,7 +0,0 @@
-native2ascii -encoding utf-8 JewishHolidaysCalendar_iw.properties ..\hebcal\JewishHolidaysCalendar_iw.properties
-native2ascii -encoding utf-8 Parshiot_iw.properties ..\hebcal\Parshiot_iw.properties
-native2ascii -encoding utf-8 Calendar_iw.properties ..\hebcal\Calendar_iw.properties
-
-native2ascii -encoding utf-8 JewishHolidaysCalendar_ru.properties ..\hebcal\JewishHolidaysCalendar_ru.properties
-native2ascii -encoding utf-8 Calendar_ru.properties ..\hebcal\Calendar_ru.properties
-native2ascii -encoding utf-8 Parshiot_ru.properties ..\hebcal\Parshiot_ru.properties
